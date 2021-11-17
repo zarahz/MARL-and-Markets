f9ac32e60b546ebf0d2ccea6bb0f708191515b3a
Zarah Zahreddin <zarah.zahreddin@gmail.com>
2021-11-17 23:13:11 +0100

    fixed stats

--- Coloring/scripts/powershell/hard/top_stats_rooms.ps1
+++ Coloring/scripts/powershell/hard/top_stats_rooms.ps1
@@ -1,32 +1,55 @@
-# PPO COOP
-python -m scripts.train --algo ppo --agents 3 --model hard_rooms/ppo/3-ppo-dr `
-    --env FourRooms-Grid-v0 `
-    --setting difference-reward `
-    --grid-size 9 `
-    --batch-size 64 `
-    --max-steps 30 `
-    --frames-per-proc 256 `
-    --frames 200000 `
-    --capture-interval 15
 
-# PPO MIXED
-python -m scripts.train --algo ppo --agents 3 --model hard_rooms/ppo/3-ppo-mixed `
-    --env FourRooms-Grid-v0 `
-    --setting mixed-motive `
-    --grid-size 9 `
-    --batch-size 64 `
-    --max-steps 30 `
-    --frames-per-proc 256 `
-    --frames 200000 `
-    --capture-interval 15
+python -m scripts.train --algo ppo --agents 1 --model hard_rooms/ppo/1-ppo `
+--env FourRooms-Grid-v0 `
+--grid-size 9 `
+--batch-size 64 `
+--frames-per-proc 256 `
+--frames 200000 `
+--capture-interval 15
 
-# PPO COMP
-python -m scripts.train --algo ppo --agents 3 --model hard_rooms/ppo/3-ppo-competitive `
+# # PPO COOP
+# python -m scripts.train --algo ppo --agents 3 --model hard_rooms/ppo/3-ppo-dr `
+#     --env FourRooms-Grid-v0 `
+#     --setting difference-reward `
+#     --grid-size 9 `
+#     --batch-size 64 `
+#     --max-steps 30 `
+#     --frames-per-proc 256 `
+#     --frames 200000 `
+#     --capture-interval 15
+
+# # PPO MIXED
+# python -m scripts.train --algo ppo --agents 3 --model hard_rooms/ppo/3-ppo-mixed `
+#     --env FourRooms-Grid-v0 `
+#     --setting mixed-motive `
+#     --grid-size 9 `
+#     --batch-size 64 `
+#     --max-steps 30 `
+#     --frames-per-proc 256 `
+#     --frames 200000 `
+#     --capture-interval 15
+
+# # PPO COMP
+# python -m scripts.train --algo ppo --agents 3 --model hard_rooms/ppo/3-ppo-competitive `
+#     --env FourRooms-Grid-v0 `
+#     --setting mixed-motive-competitive `
+#     --grid-size 9 `
+#     --batch-size 64 `
+#     --max-steps 30 `
+#     --frames-per-proc 256 `
+#     --frames 200000 `
+#     --capture-interval 15
+
+
+
+python -m scripts.train --algo dqn --agents 1 --model hard_rooms/dqn/1-dqn `
     --env FourRooms-Grid-v0 `
-    --setting mixed-motive-competitive `
-    --grid-size 9 `
     --batch-size 64 `
-    --max-steps 30 `
+    --initial-target-update 1000 `
+    --target-update 10000 `
+    --replay-size 700000 `
+    --epsilon-decay 20000 `
+    --grid-size 9 `
     --frames-per-proc 256 `
     --frames 200000 `
     --capture-interval 15
